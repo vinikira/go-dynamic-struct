6e2e9ba2facaa6a9e2afe6cc0af9eb344952159a
Vinícius Simões <vinicius.simoes95@gmail.com>
2016-08-29 17:47:24 -0300

    forma alternativa de fazer o dynamic struct

--- API/person.go
+++ API/person.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Person struct {
-	Nome   string `json:"nome"`
-	Email  string `json:"email"`
-	Sexo   string `json:"sexo"`
-	Idade  int    `json:"idade"`
+	Nome   string
+	Email  string
+	Sexo   string
+	Idade  int
 	Outros map[string]interface{}
 }
 
@@ -33,21 +33,28 @@ func GetPeople(url string) ([]Person, error) {
 		return []Person{}, err
 	}
 
-	if err = json.Unmarshal(body, &p); err != nil {
-		return []Person{}, err
-	}
-
 	if err = json.Unmarshal(body, &others); err != nil {
 		return []Person{}, err
 	}
 
-	for i := range p {
-		p[i].Outros = make(map[string]interface{})
-		for k, v := range others[i] {
-			if k != "nome" && k != "email" && k != "sexo" && k != "idade" {
-				p[i].Outros[k] = v
+	for _, v := range others {
+		var per Person
+		per.Outros = make(map[string]interface{})
+		for k, val := range v {
+			switch k {
+			case "nome":
+				per.Nome = val.(string)
+			case "email":
+				per.Email = val.(string)
+			case "sexo":
+				per.Sexo = val.(string)
+			case "idade":
+				per.Idade = int(val.(float64))
+			default:
+				per.Outros[k] = val
 			}
 		}
+		p = append(p, per)
 	}
 
 	return p, nil
